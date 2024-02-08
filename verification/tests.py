cdbb57cea5e9e43e089ea40497fa89a7f5494f86
Oleksandr Zozulia <freemanlex@gmail.com>
2024-02-08 18:49:06 +0200

    "first commit"

---
+++ verification/tests.py
@@ -0,0 +1,80 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ["sMcigkqow"],
+            "answer": ((0, 0, 1, 0, 0),
+ (0, 1, 0, 1, 0),
+ (1, 0, 2, 0, 1),
+ (0, 1, 0, 1, 0),
+ (0, 0, 1, 0, 0)),
+        },
+        {
+            "input": ["acSyIwoQkumGe"],
+            "answer": ((1, 0, 1, 0, 1),
+ (0, 2, 0, 2, 0),
+ (1, 0, 1, 0, 1),
+ (0, 2, 0, 2, 0),
+ (1, 0, 1, 0, 1)),
+        },
+    ],
+    "Extra": [
+        {
+            "input": ["cdeyfkmphrtAjovuw"],
+            "answer": ((2, 0, 1, 1, 1),
+ (1, 0, 1, 0, 1),
+ (1, 0, 1, 0, 1),
+ (1, 0, 1, 0, 1),
+ (1, 1, 1, 0, 1)),
+        },
+        {
+            "input": ["cdebJIFGHab"],
+            "answer": ((1, 1, 1, 1, 1),
+ (2, 2, 2, 2, 2),
+ (0, 0, 0, 0, 0),
+ (0, 0, 0, 0, 0),
+ (0, 0, 0, 0, 0)),
+        },
+        {
+            "input": ["QgysUaEI"],
+            "answer": ((1, 0, 0, 0, 2),
+ (0, 1, 0, 2, 0),
+ (0, 0, 0, 0, 0),
+ (0, 2, 0, 1, 0),
+ (2, 0, 0, 0, 1)),
+        },
+        {
+            "input": ["GKbJNqxuRmYVtfCei"],
+            "answer": ((0, 1, 2, 0, 1),
+ (1, 2, 0, 1, 2),
+ (2, 0, 1, 2, 0),
+ (0, 1, 2, 0, 1),
+ (1, 2, 0, 1, 2)),
+        },
+        {
+            "input": [""],
+            "answer": ((0, 0, 0, 0, 0),
+ (0, 0, 0, 0, 0),
+ (0, 0, 0, 0, 0),
+ (0, 0, 0, 0, 0),
+ (0, 0, 0, 0, 0)),
+        },
+        {
+            "input": ["ajkogqivxyfdcsuhwplntrebm"],
+            "answer": ((1, 1, 1, 1, 1),
+ (1, 1, 1, 1, 1),
+ (1, 1, 1, 1, 1),
+ (1, 1, 1, 1, 1),
+ (1, 1, 1, 1, 1)),
+        },
+    ]
+}
